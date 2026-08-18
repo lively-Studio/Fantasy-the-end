309970404fcc0aa36486a4400c82da046982c8b4
cangcang <cangcang_code@icloud.com>
2026-08-18 11:23:28 +0800

    feat: 完全替换原版末影人为可驯服末影人
    通过 Mixin 注入 EntityType.create()，所有原版末影人（自然生成、命令、
    刷怪蛋等）都会创建为 TameableEnderManEntity：
    - 移除 EndermanEntityTameMixin（不再需要）
    - 新增 EntityTypeFactoryMixin 替换 EntityType.ENDERMAN 工厂
    - 手持紫松果右键即可驯服任意末影人

---
+++ src/main/java/com/fantasy/end/mixin/EntityTypeFactoryMixin.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2026 cangcang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.fantasy.end.mixin;
+
+import com.fantasy.end.entity.TameableEnderManEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EntityType.class)
+public abstract class EntityTypeFactoryMixin {
+
+    @SuppressWarnings("unchecked")
+    @Inject(method = "create", at = @At("HEAD"), cancellable = true)
+    private void onCreate(World world, CallbackInfoReturnable<Entity> cir) {
+        EntityType<?> self = (EntityType<?>) (Object) this;
+        // 替换所有原版末影人生成为可驯服末影人
+        if (self == EntityType.ENDERMAN) {
+            Entity entity = new TameableEnderManEntity(
+                    (EntityType<? extends EndermanEntity>) (EntityType<?>) EntityType.ENDERMAN,
+                    world
+            );
+            cir.setReturnValue(entity);
+        }
+    }
+}
